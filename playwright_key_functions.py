cf994e81cd7f55608d3c00a2af23518761547317
JefriJerina <theaijeffrey@gmail.com>
2025-09-10 14:37:27 +0530

    Add files via upload

--- playwright_key_functions.py
+++ playwright_key_functions.py
@@ -1,2 +1,8 @@
 from playwright.async_api import async_playwright
 
+async def playwright_function():
+    async with async_playwright()
+    browser=await p.chromium.launch()
+    pages=await browser.page()
+    
+    
